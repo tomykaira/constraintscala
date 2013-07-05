e8426db5dc3a8677d1996ba5aeea7bd103785609
tomykaira <tomykaira@gmail.com>
2013-07-05 10:22:10 +0900

    Diff of all files

--- src/main/scala/com/tomykaira/uchronie/CommitDecorator.scala
+++ src/main/scala/com/tomykaira/uchronie/CommitDecorator.scala
@@ -5,4 +5,9 @@ import org.eclipse.jgit.revwalk.RevCommit
 case class CommitDecorator(revCommit: RevCommit) {
   def tableRow(repository: GitRepository): Array[AnyRef] =
     Array(repository.abbreviate(revCommit.getId).name, revCommit.getShortMessage)
+
+  def fullDiff(repository: GitRepository): String = {
+    val diffs = repository.diff(revCommit)
+    diffs.map(new DiffDecorator(_).formatted(repository)).mkString("\n\n")
+  }
 }
