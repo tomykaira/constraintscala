bf3ffe943fcc48dfb7c7226ee4292630f4540b99
tomykaira <tomykaira@gmail.com>
2013-07-07 13:26:15 +0900

    Refactor applyOperation

--- src/main/scala/com/tomykaira/uchronie/git/CommitThread.scala
+++ src/main/scala/com/tomykaira/uchronie/git/CommitThread.scala
@@ -23,28 +23,26 @@ object CommitThread {
 
 trait CommitThread {
   val commits: List[VirtualCommit]
+  type OperationResult = Either[CommitThread.Error, CommitThread]
 
-  def applyOperation(op: Operation): Either[CommitThread.Error, CommitThread] = op match {
+  def applyOperation(op: Operation): OperationResult = op match {
     case RenameOp(target, message) =>
-      commits.indexOf(target) match {
-        case -1 => Left(CommitThread.CommitNotFound(this, op, target))
-        case index: Int =>
-          val newCommits =
-            pick(commits.take(index)) ++ (VirtualCommit.Rename(target, message) :: commits.drop(index + 1))
-          Right(CommitThread.fromVirtualCommits(newCommits))
+      withTargetIndex(target, op).right.flatMap { index =>
+        result(pick(commits.take(index)) ++ (VirtualCommit.Rename(target, message) :: commits.drop(index + 1)))
+      }
+    case DeleteOp(target) =>
+      withTargetIndex(target, op).right.flatMap { index =>
+        result(pick(commits.take(index)) ++ commits.drop(index + 1))
       }
     case MoveOp(targets, pos) =>
-      val indices = targets.map(commits.indexOf(_))
-      val notFound = indices.indexOf(-1)
-      if (notFound != -1)
-        Left(CommitThread.CommitNotFound(this, op, targets(notFound)))
-      else {
+      if (targets.isEmpty)
+        return Right(this)
+      indicesInList(targets, op).right.flatMap { indices =>
         val lastTarget = indices.max
         val picked = pick(commits.take(lastTarget)) ++ commits.drop(lastTarget)
-        val newCommits = picked.take(pos).filterNot(p => targets.exists(t => p derived t)) ++
+        result(picked.take(pos).filterNot(p => targets.exists(t => p derived t)) ++
           move(targets) ++
-          picked.drop(pos).filterNot(p => targets.exists(t => p derived t))
-        Right(CommitThread.fromVirtualCommits(newCommits))
+          picked.drop(pos).filterNot(p => targets.exists(t => p derived t)))
       }
     case SquashOp(targets, message) =>
       if (targets.isEmpty) {
@@ -53,19 +51,30 @@ trait CommitThread {
         val firstIndex = commits.indexOf(targets.head)
         val lastIndex = firstIndex + targets.length
         val newMessage = message.getOrElse(targets.reverse.map(_.message.stripLineEnd).mkString("\n\n"))
-        val newCommits = pick(commits.take(firstIndex)) ++ (VirtualCommit.Squash(targets, newMessage) :: commits.drop(lastIndex))
-        Right(CommitThread.fromVirtualCommits(newCommits))
+        result(pick(commits.take(firstIndex)) ++ (VirtualCommit.Squash(targets, newMessage) :: commits.drop(lastIndex)))
       } else {
         Left(CommitThread.NotSequentialSlice(this, op))
       }
-    case DeleteOp(target) =>
-      commits.indexOf(target) match {
-        case -1 => Left(CommitThread.CommitNotFound(this, op, target))
-        case index: Int =>
-          val newCommits = pick(commits.take(index)) ++ commits.drop(index + 1)
-          Right(CommitThread.fromVirtualCommits(newCommits))
-      }
   }
+
+  private def indicesInList(targets: List[VirtualCommit], op: Operation): Either[CommitThread.Error, List[Int]] = {
+    val indices = targets.map(commits.indexOf(_))
+    val notFound = indices.indexOf(-1)
+    if (notFound != -1) {
+      Left(CommitThread.CommitNotFound(this, op, targets(notFound)))
+    } else {
+      Right(indices)
+    }
+  }
+
+  private def withTargetIndex(target: VirtualCommit, op: Operation) =
+    commits.indexOf(target) match {
+      case -1 => Left(CommitThread.CommitNotFound(this, op, target))
+      case index: Int => Right(index)
+    }
+
+  private def result(commits: List[VirtualCommit]) =
+    Right(CommitThread.fromVirtualCommits(commits))
 
   private def pick(cs: List[VirtualCommit]) = cs map VirtualCommit.Pick
   private def move(cs: List[VirtualCommit]) = cs map VirtualCommit.Move
