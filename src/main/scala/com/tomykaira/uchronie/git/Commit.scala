26bbe1b2c9a6442d586245865e260f2cb833337d
tomykaira <tomykaira@gmail.com>
2013-07-07 13:56:57 +0900

    Define simplification of commit

--- src/main/scala/com/tomykaira/uchronie/git/Commit.scala
+++ src/main/scala/com/tomykaira/uchronie/git/Commit.scala
@@ -10,33 +10,61 @@ object Commit {
 sealed trait VirtualCommit {
   def derived(commit: VirtualCommit): Boolean
   val message: String
+  def simplify: VirtualCommit
 }
 sealed trait Commit extends VirtualCommit {
   val raw: RevCommit
   val message = raw.getFullMessage
+  def simplify: VirtualCommit = this
 }
 object VirtualCommit {
   case class Pick(previous: VirtualCommit) extends VirtualCommit {
     def derived(commit: VirtualCommit) = this == commit || (previous derived commit)
 
     val message: String = previous.message
+
+    override def simplify: VirtualCommit =
+      previous.simplify match {
+        case it @ (Pick(_) | Rename(_,_)) => it
+        case it => Pick(it)
+      }
   }
+
   case class Rename(previous: VirtualCommit, message: String) extends VirtualCommit {
     def derived(commit: VirtualCommit) = this == commit || (previous derived commit)
+
+    override def simplify: VirtualCommit =
+      previous.simplify match {
+        case Pick(c) => Rename(c, message)
+        case Rename(c, _) => Rename(c, message)
+        case it => Rename(it, message)
+      }
   }
+
   case class Move(previous: VirtualCommit) extends VirtualCommit {
     def derived(commit: VirtualCommit) = this == commit || (previous derived commit)
 
     val message: String = previous.message
+
+    override def simplify: VirtualCommit =
+      previous.simplify match {
+        case Pick(c) => Move(c)
+        case it => Move(it)
+      }
   }
+
   case class Squash(previous: List[VirtualCommit], message: String) extends VirtualCommit {
     def derived(commit: VirtualCommit) = this == commit || previous.exists(_ derived commit)
+
+    override def simplify: VirtualCommit =
+      Squash(previous.map(_.simplify), message)
   }
 
   // for testing
   case class DummyCommit(id: Int) extends VirtualCommit{
     def derived(commit: VirtualCommit) = this == commit
     val message = s"Dummy $id"
+    def simplify: VirtualCommit = this
   }
 }
 case class RawCommit(raw: RevCommit) extends Commit {
---
+++ src/test/scala/com/tomykaira/uchronie/git/CommitSpec.scala
@@ -0,0 +1,43 @@
+package com.tomykaira.uchronie.git
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSpec
+import com.tomykaira.uchronie.git.VirtualCommit._
+
+class CommitSpec extends FunSpec with ShouldMatchers {
+  describe("simplify") {
+    val core = DummyCommit(3)
+    describe("Pick") {
+      it("should simplify enclosing Pick") {
+        Pick(Pick(core)).simplify should equal (Pick(core))
+      }
+      it("should simplify two enclosing Pick") {
+        Pick(Pick(Pick(core))).simplify should equal (Pick(core))
+      }
+      it("should simplify Rename") {
+        Pick(Rename(core, "A")).simplify should equal (Rename(core, "A"))
+      }
+    }
+    describe("Rename") {
+      it("should simplify sequential Rename") {
+        Rename(Rename(core, "A"), "B").simplify should equal (Rename(core, "B"))
+      }
+      it("should omit Pick") {
+        Rename(Pick(Pick(core)), "B").simplify should equal (Rename(core, "B"))
+      }
+    }
+    describe("Move") {
+      it("should simplify internal Picks") {
+        Move(Pick(Pick(core))).simplify should equal (Move(core))
+      }
+      it("should simplify internal") {
+        Move(Pick(Rename(Pick(core),"A"))).simplify should equal (Move(Rename(core, "A")))
+      }
+    }
+    describe("squash") {
+      it("should simplify each commit") {
+        VirtualCommit.Squash(List(Pick(Pick(core))), "Foo").simplify should equal (VirtualCommit.Squash(List(Pick(core)), "Foo"))
+      }
+    }
+  }
+}
