f5bd06dcadc2d7c593665e747baada67dbf7e5b5
tomykaira <tomykaira@gmail.com>
2013-07-08 23:06:03 +0900

    Create threads container

---
+++ src/main/scala/com/tomykaira/uchronie/git/ThreadTransition.scala
@@ -0,0 +1,22 @@
+package com.tomykaira.uchronie.git
+
+import scala.collection.mutable.ListBuffer
+
+class ThreadTransition(initial: CommitThread) {
+  var current: CommitThread = initial
+
+  val history: ListBuffer[(CommitThread, Operation)] = ListBuffer()
+
+  def transit(op: Operation): Either[CommitThread.Error, CommitThread] = {
+    val result = current.applyOperation(op)
+    result.right.foreach { next =>
+      (current, op) +=: history
+      current = next
+    }
+    result
+  }
+
+  def threads: List[CommitThread] = current :: history.map(_._1).toList
+
+  def operations: List[Operation] = history.map(_._2).toList
+}
