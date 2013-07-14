c55910eacd3c09f774c56ad910dd012943ead91a
tomykaira <tomykaira@gmail.com>
2013-07-15 00:35:39 +0900

    Reset current branch on initError

--- uchronie/src/main/scala/com/tomykaira/uchronie/Main.scala
+++ uchronie/src/main/scala/com/tomykaira/uchronie/Main.scala
@@ -42,24 +42,25 @@ object Main extends SimpleSwingApplication {
 
   override def main(args: Array[String]) {
     new ArgumentParser(args).parse match {
-      case Left(e) => initializationError(e)
+      case Left(e) => initializationError(e, None)
       case Right(parsed) =>
         repository = new GitRepository(parsed.repository)
         if (!repository.isClean)
-          initializationError("Repository is not clean.  Commit everything before start uchronie for safety.")
+          initializationError("Repository is not clean.  Commit everything before start uchronie for safety.", Some(repository))
         repository.resolve(parsed.start) match {
           case Some(id) => start = id
-          case None => initializationError("Start SHA-1 " + parsed.start + " is not resolved to one object id")
+          case None => initializationError("Start SHA-1 " + parsed.start + " is not resolved to one object id", Some(repository))
         }
         repository.resolve(parsed.end) match {
           case Some(id) => end = id
-          case None => initializationError("End SHA-1 " + parsed.end + " is not resolved to one object id")
+          case None => initializationError("End SHA-1 " + parsed.end + " is not resolved to one object id", Some(repository))
         }
     }
     super.main(args)
   }
 
-  def initializationError(message: String) {
+  def initializationError(message: String, repo: Option[GitRepository]) {
+    repo foreach { repo => repo.resetToOriginalBranch(repo.head) }
     System.err.println(message)
     System.exit(1)
   }
