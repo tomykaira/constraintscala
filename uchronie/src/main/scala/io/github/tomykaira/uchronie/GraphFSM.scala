25bdb99ced0865182020717fa581d90e1bd8c571
tomykaira <tomykaira@gmail.com>
2013-07-27 07:47:13 +0900

    Pass callback to Actor instead of using Future
    `ask` does not allow infinite timeout length.
    (https://groups.google.com/forum/#!topic/akka-user/fWxZ5-gCEBA and https://www.assembla.com/spaces/akka/tickets/1883)
    
    To remove timeout specification, actor should be used with `tell` style.
    In this case, there is no problem to process callback on actor's thread.
    Therefore it is changed to use Request object to contain success and failure callbacks, and the operation target.
    
    This fixes #3.

--- uchronie/src/main/scala/io/github/tomykaira/uchronie/GraphFSM.scala
+++ uchronie/src/main/scala/io/github/tomykaira/uchronie/GraphFSM.scala
@@ -3,11 +3,7 @@ package io.github.tomykaira.uchronie
 import io.github.tomykaira.uchronie.ui.GraphState
 import io.github.tomykaira.constraintscala.FSM
 import akka.actor.{Props, ActorSystem}
-import akka.util.Timeout
-import scala.concurrent.duration._
-import akka.pattern.ask
-import scala.concurrent.ExecutionContext.Implicits.global
-import io.github.tomykaira.uchronie.git.{Operation, ArrangingGraph, Worker}
+import io.github.tomykaira.uchronie.git.{Request, Operation, ArrangingGraph, Worker}
 import scala.swing.Dialog
 import org.eclipse.jgit.lib.ObjectId
 
@@ -16,24 +12,22 @@ class GraphFSM(repository: GitRepository, start: ObjectId, last: ObjectId) exten
 
   private[this] val system = ActorSystem("Worker")
   private[this] val actor = system.actorOf(Props[Worker])
-  private[this] implicit val timeout = Timeout(60.seconds)
 
   onChange {
     case GraphState.Applying(graph) =>
-      val future = ask(actor, graph).mapTo[ArrangingGraph]
-      future.onSuccess {
-        case g: ArrangingGraph => changeStateTo(GraphState(g))
-      }
-      future.onFailure {
-        case err =>
-          changeStateTo(GraphState.Modified(graph))
-          Dialog.showMessage(title = "Error", message = err.getMessage)
-      }
+      actor ! Request(graph, { g =>
+        changeStateTo(GraphState(g))
+      }, { err =>
+        changeStateTo(GraphState.Modified(graph))
+        Dialog.showMessage(title = "Error", message = err.getMessage)
+      })
     case GraphState.Editing(manager) =>
-      ask(actor, manager).mapTo[ArrangingGraph] onSuccess {
-        case result: ArrangingGraph.Clean =>
-          changeStateTo(GraphState.Clean(result))
-      }
+      actor ! Request(manager, { g =>
+        changeStateTo(GraphState(g))
+      }, { err =>
+        changeStateTo(GraphState.Clean(manager.graph))
+        Dialog.showMessage(title = "Error", message = err.getMessage)
+      })
     case _ =>
   }
 
